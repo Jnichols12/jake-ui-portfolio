6e934c411e1825b1f97b80cc8bd646245cb73d7d
Jake <jnichols12@huskers.unl.edu>
2024-09-23 13:41:55 -0500

    Deploy website - based on eca184e85e5d63233f7037149b8e3ce4a81b3864

---
+++ assets/js/814f3328.980eb907.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Aesthetic Design - How to Design Effective Slides","permalink":"/jake-ui-portfolio/blog/aesthetic-design","unlisted":false}]}')}}]);
