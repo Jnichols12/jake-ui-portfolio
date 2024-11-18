42e980370f40468c2a20ad1db246e791a5d82b71
Jake Nichols <jnichols12@huskers.unl.edu>
2024-11-17 20:02:53 -0600

    Deploy website - based on 5830573fc0a1f7a14e3caecc6742753163ffd9b5

---
+++ assets/js/4f55f153.b1236c89.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9848],{1918:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/jake-ui-portfolio/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":14,"previousPage":"/jake-ui-portfolio/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
